b0fe470b2ea881142e6f97e9461a1e0edadc160b
Adib <adibshadman857@gmail.com>
2020-04-16 17:27:04 +1000

    TweetHandler and connection to worker client database

--- src/upson/grant/Database.java
+++ src/upson/grant/Database.java
@@ -1,6 +1,44 @@
 package upson.grant;
+/*
+  @author Grant Upson : 385831
+  @author Adib Shadman : 468684
+ */
+import java.sql.*;
 
 public class Database
 {
+    private final String url = "jdbc:mysql://localhost:3306/tweets?serverTimezone=Australia/Melbourne";
+
+    private final String username;
+    private final String password;
+
+    public Database(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+
+
+    public void insertIntoDatabase(Tweet tweet)
+    {
+        try(Connection connection = DriverManager.getConnection(url, this.username, this.password))
+        {
+            String query = "Insert into tweets(tweet_id, sentiment, airline, message, date_created) values (?, ?, ?, ?, ?)";
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, tweet.getTweetID());
+            statement.setString(2, tweet.getSentiment());
+            statement.setString(3, tweet.getAirline());
+            statement.setString(4, tweet.getMessage());
+            statement.setString(5, tweet.getDateCreated());
+
+            statement.executeUpdate();
+        }
+        catch(SQLException sqlException)
+        {
+            System.out.println("Error " + sqlException.getMessage());
+        }
+    }
+
 
 }
--- src/upson/grant/TweetHandler.java
+++ src/upson/grant/TweetHandler.java
@@ -1,5 +1,22 @@
 package upson.grant;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+/*
+@author Grant Upson : 385831
+@author Adib Shadman : 468684
+ */
 public class TweetHandler implements Runnable
 {
     private String hostname;
@@ -11,12 +28,31 @@ public class TweetHandler implements Runnable
         this.port = port;
     }
 
+
     @Override
     public void run()
     {
-        while(true)
+        try(Socket serverConnection = new Socket(hostname, port))
         {
-            System.out.println("Tweet Handler!");
+            while (true)
+            {
+                String date = "2020-03-01";
+                ObjectInputStream tweet_inStream = new ObjectInputStream(serverConnection.getInputStream());
+                //Tweet tweet = (Tweet)tweet_inStream.readObject();
+                DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+                Date myDate = formatter.parse(date);
+                java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
+                Tweet tweet = new Tweet(1, "Hello", "World", "jet star", myDate);
+                Database tweet_database = new Database("root", "");
+                tweet_database.insertIntoDatabase(tweet);
+                System.out.println("Tweet Handler!");
+            }
         }
+        catch(IOException | ParseException exception)
+        {
+            System.out.println("Error: " + exception.getMessage());
+        }
+
     }
 }
